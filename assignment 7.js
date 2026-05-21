5a7ab2d96b99223f04e24f3ceaa214d6ba0806df
isratjahan4219-cpu <isratjahan4219@gmail.com>
2026-05-21 21:50:17 +0600

    Add files via upload

---
+++ assignment 7.js
@@ -0,0 +1,48 @@
+
+// Variables
+let studentName = "ZENO";
+let age = 20;
+let courseName = "JavaScript Basics";
+let isEnrolled = true;
+
+
+console.log("Student Name:", studentName);
+console.log("Age:", age);
+console.log("Course Name:", courseName);
+console.log("Is Enrolled:", isEnrolled);
+
+
+
+// Number variables
+let num1 = 10;
+let num2 = 5;
+
+
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+
+
+
+console.log("For Loop (1-10):");
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+
+console.log("While Loop (10-1):");
+let j = 10;
+
+while (j >= 1) {
+    console.log(j);
+    j--;
+}
+
+
+
+function helloWorld() {
+    return "Hello World";
+}
+
+console.log(helloWorld());
